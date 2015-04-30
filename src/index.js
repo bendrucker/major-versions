1401e4aa5d881f46bcbff535e2265f163c0a1044
Ben Drucker <bvdrucker@gmail.com>
2015-04-30 19:59:31 -0400

    Throw when an unbounded range is passed

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import assert from 'assert'
 import {Range} from 'semver'
 import truncate from 'semver-truncate'
 import numberRange from 'range-function'
+import last from 'array-last'
 
 export default function majors (range) {
-  const [lower, upper] = new Range(range).set
+  const semvers = new Range(range).set
     .reduce((comparators, set) => {
       comparators.push.apply(comparators, set)
       return comparators
@@ -14,13 +15,18 @@ export default function majors (range) {
     .map((comparator) => {
       return {
         version: parseInt(comparator.semver.version),
-        operator: comparator.semver.operator
+        operator: comparator.operator
       }
     })
     .sort((a, b) => {
       return a.version < b.version ? -1 : 1
     })
-    .map(semver => semver.version)
+
+  if (last(semvers).operator.charAt(0) === '>') {
+    throw new Error(`Cannot determine major versions: "${range}" is unbounded`)
+  }
+
+  const [lower, upper] = semvers.map(semver => semver.version)
 
   return numberRange(lower, upper).map(n => n.toString())
 }
--- test/index.js
+++ test/index.js
@@ -7,6 +7,8 @@ test((t) => {
   t.deepEqual(majors('> 2 < 5'), ['3', '4'])
   t.deepEqual(majors('< 5 > 2'), ['3', '4'])
   t.deepEqual(majors('>= 2.3.0 < 5'), ['2', '3', '4'])
+  t.throws(majors.bind(null, '> 2 > 10'), /unbounded/)
+  t.throws(majors.bind(null, '> 2'), /unbounded/)
   t.end()
 })
 
