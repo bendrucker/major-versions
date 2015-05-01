f2c23cb3d93ba970194b74da8cd316f2c18bc272
Ben Drucker <bvdrucker@gmail.com>
2015-04-30 20:45:55 -0400

    Add test for = operator

--- test/index.js
+++ test/index.js
@@ -4,6 +4,7 @@ import test from 'tape'
 import majors from '../'
 
 test((t) => {
+  t.deepEqual(majors('2'), ['2'])
   t.deepEqual(majors('> 2 < 5'), ['3', '4'])
   t.deepEqual(majors('< 5 > 2'), ['3', '4'])
   t.deepEqual(majors('>= 2.3.0 < 5'), ['2', '3', '4'])
