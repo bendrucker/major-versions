be46000bb7be4eb1ca06f262ffcaaca51c4c1c16
Ben Drucker <bvdrucker@gmail.com>
2015-04-30 21:22:18 -0400

    Code cleanup

--- src/index.js
+++ src/index.js
@@ -5,34 +5,19 @@ import {Range} from 'semver'
 import numberRange from 'range-function'
 import last from 'array-last'
 import uniqueConcat from 'unique-concat'
+import sortOn from 'sort-on'
 
 export default function majors (range, maximum) {
   return new Range(range).set
     .map((comparators) => {
-      const semvers = comparators.map((comparator) => {
+      const semvers = sortOn(comparators.map((comparator) => {
         return {
           version: parseInt(comparator.semver.version),
           operator: comparator.operator
         }
-      })
-      .sort((a, b) => {
-        return a.version < b.version ? -1 : 1
-      })
-      if (last(semvers).operator.charAt(0) === '>') {
-        if (maximum == null) {
-          throw new Error(`Cannot determine major versions: "${range}" is unbounded and no maximum was provided`)
-        }
-        semvers.push({
-          version: parseInt(maximum) + 1,
-          operator: '<'
-        })
-      }
-      if (semvers[0].operator.charAt(0) === '<') {
-        semvers.push({
-          version: 0,
-          operator: '>='
-        })
-      }
+      }), 'version')
+      upperBound(semvers, range, maximum)
+      lowerBound(semvers)
       const [lower, upper] = semvers.map(semver => semver.version)
       return numberRange(lower, upper)
     })
@@ -41,4 +26,25 @@ export default function majors (range, maximum) {
     })
     .sort()
     .map(n => n.toString())
+}
+
+function upperBound (semvers, range, maximum) {
+  if (last(semvers).operator === '>=') {
+    if (maximum == null) {
+      throw new Error(`Cannot determine major versions: "${range}" is unbounded and no maximum was provided`)
+    }
+    semvers.push({
+      version: parseInt(maximum) + 1,
+      operator: '<'
+    })
+  }
+}
+
+function lowerBound (semvers) {
+  if (semvers[0].operator === '<') {
+    semvers.push({
+      version: 0,
+      operator: '>='
+    })
+  }
 }
